64c4c11b1d5bfc735386d8d9d15b946887fb8bd0
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-06 15:13:15 -0500

    add action generator

---
+++ src/store/actions/actions.js
@@ -0,0 +1,56 @@
+import * as ACTION_TYPES from './action-types';
+
+export const SUCCESS = {
+  type: ACTION_TYPES.SUCCESS
+}
+
+export const FAILURE = {
+  type: ACTION_TYPES.FAILURE
+}
+
+
+export const success = () => {
+  return {
+    type: ACTION_TYPES.SUCCESS
+  }
+}
+
+export const failure = () => {
+  return {
+    type: ACTION_TYPES.FAILURE
+  }
+}
+
+export const userInput = (text) => {
+  return {
+    type: ACTION_TYPES.USER_INPUT,
+    payload: text
+  }
+}
+
+
+export const loginSuccess = () => {
+  return {
+    type: ACTION_TYPES.LOGIN_SUCCESS
+  }
+}
+
+export const loginFailure = () => {
+  return {
+    type: ACTION_TYPES.LOGIN_FAILURE
+  }
+}
+
+
+export const addProfile = (profile) => {
+  return {
+    type: ACTION_TYPES.ADD_PROFILE,
+    payload: profile
+  }
+}
+
+export const removeProfile = () => {
+  return {
+    type: ACTION_TYPES.REMOVE_PROFILE
+  }
+}
