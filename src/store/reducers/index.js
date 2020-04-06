91ca47f72022e983d4ecfa32f75af94795075111
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-06 16:22:05 -0500

    add postReducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,12 +1,14 @@
 
 import AuthReducer from './authReducer';
 import UserReducer from './userReducer';
+import PostReducer from './postReducer';
 import { combineReducers } from 'redux';
 
 
 const rootReducer = combineReducers({
   authReducer: AuthReducer,
   userReducer: UserReducer,
+  postReducer: PostReducer,
 });
 
 export default rootReducer;
