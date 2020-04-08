9a522d505efaacbe08c5a62356490e4010b9fe7e
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-08 16:59:52 -0500

    add form into AddPost component

--- src/store/components/blog/addPost.js
+++ src/store/components/blog/addPost.js
@@ -1,10 +1,30 @@
 import React, {Component} from 'react';
+import { TextField } from '@material-ui/core';
 
 class AddPost extends Component {
+    handleSubmit() {
+        console.log("handle submit");
+    }
     render() {
         return (
             <div>
-                ADD POST
+                <form onSubmit={this.handleSubmit}>
+                    <TextField 
+                        id='title'
+                        label='Title'
+                        margin='normal'
+                    />
+                    <br />
+                    <TextField 
+                        id='body'
+                        label='Body'
+                        multiline
+                        rows='4'
+                        margin='normal'
+                    />
+                    <br />
+                    <button type='submit' >Submit</button>
+                </form>
             </div>
         );
     }
