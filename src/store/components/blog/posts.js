6b945b5bb2a0e55289fb4afd2d4c01801f31e4c4
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-08 19:41:42 -0500

    add posts component

---
+++ src/store/components/blog/posts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Posts extends Component {
+    render() {
+        return (
+            <div>
+                POSTS
+            </div>
+        );
+    }
+}
+
+export default Posts;
