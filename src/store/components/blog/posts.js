997ef4ee058c51bae227dd7a55fd902010772016
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-08 19:54:16 -0500

    add RenderPosts subcomponent

--- src/store/components/blog/posts.js
+++ src/store/components/blog/posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
     Table,
     TableBody,
@@ -8,6 +9,21 @@ import {
     TableRow,
     Paper
 } from '@material-ui/core';
+
+const RenderPosts = post => (
+    <TableRow>
+        <TableCell>
+            <Link to={{
+                pathname: '/post/' + post.post.pid,
+                state: {post}
+            }} >
+                <h4>
+                    {post.post.title}
+                </h4>
+            </Link>
+        </TableCell>
+    </TableRow>
+);
 
 class Posts extends Component {
     render() {
