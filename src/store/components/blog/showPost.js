fb9205af351ea64366430f727c55594f2a2aa7f4
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-09 10:16:21 -0500

    add RenderComments

--- src/store/components/blog/showPost.js
+++ src/store/components/blog/showPost.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
+import { Button } from '@material-ui/core';
+
+const RenderComments = (comment, userId) => (
+    <div>
+        <h3>{comment.comment}</h3>
+        <small>{comment.date_created}</small>
+        <p>By: {comment.author} </p>
+        {
+            comment.user_id === userId
+            ?   <Button onClick={() => console.log('EDIT')} >
+                Edit
+            </Button> : null
+        }
+    </div>
+);
 
 class ShowPost extends Component {
     render() {
