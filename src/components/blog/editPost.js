c434eecca10a94c992114da2b6acdd18fc4b1fba
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-10 14:04:40 -0500

    init EditPost component

---
+++ src/components/blog/editPost.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditPost extends Component {
+    render() {
+        return (
+            <div>
+                EDIT POST
+            </div>
+        );
+    }
+}
+
+export default EditPost;
