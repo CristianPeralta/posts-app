fec8b21f7e611eeff95ef0db38e96646e06e7f13
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-10 14:10:36 -0500

    add form into

--- src/components/blog/editPost.js
+++ src/components/blog/editPost.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
+import {
+    TextField
+} from '@material-ui/core';
 
 class EditPost extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: '',
+            body: ''
+        };
+    }
     render() {
         return (
             <div>
-                EDIT POST
+                <form>
+                    <TextField
+                        id='title'
+                        label='Title'
+                        margin='normal'
+                    />
+                    <TextField
+                        id='body'
+                        label='Body'
+                        multiline
+                        rows='4'
+                        margin='normal'
+                    />
+                </form>
             </div>
         );
     }
