462755436bd026504e42845566daac6863e05e91
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-17 13:58:06 -0500

    add SignUp component

--- src/components/blog/showPost.js
+++ src/components/blog/showPost.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import * as ACTIONS from '../../store/actions/actions';
 import { connect } from 'react-redux';
---
+++ src/functional/signup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignUp = props => (
+    <div>
+        SignUp
+    </div>
+);
+
+export default SignUp;
