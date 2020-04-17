bbbf3e0ca121d66a50dc89cca160745eb83b38ab
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-17 14:04:41 -0500

    update SignUp component

--- src/functional/signup.js
+++ src/functional/signup.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 const SignUp = props => (
     <div>
-        SignUp
+        <h1>
+            SignUp and Create and Account
+        </h1>
+        <Button coloe='primary' size='large' variant='contained' onClick={() => props.auth.login()}>
+            Signup
+        </Button>
     </div>
 );
 
