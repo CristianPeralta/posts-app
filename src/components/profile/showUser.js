bba83020e6dafb202d26357206483a120bb39637
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-21 11:29:25 -0500

    add ShowUser component

---
+++ src/components/profile/showUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowUser extends Component {
+    render() {
+        return (
+            <div>
+                SHOW USER
+            </div>
+        );
+    }
+}
+
+export default ShowUser;
