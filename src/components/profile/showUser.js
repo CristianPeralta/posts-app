fb1eecb670e66f5157518bb88dc05e93e8bb17ae
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-21 12:04:18 -0500

    add RenderProfile

--- src/components/profile/showUser.js
+++ src/components/profile/showUser.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
+const RenderProfile = props => {
+    return (
+        <div className='FlexRow'>
+            <h1>
+                {props.profile.username}
+            </h1>
+        </div>
+    );
+};
+
 class ShowUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile : null
+        }
+    }
     render() {
         return (
             <div>
-                SHOW USER
+                <RenderProfile profile={this.state.profile} />
             </div>
         );
     }
