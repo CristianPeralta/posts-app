b15152ba7ef2fceaaa3a700b03aed41bd9517d7f
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-22 11:45:11 -0500

    add new action creators

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -142,3 +142,16 @@ export const removeOtherUserPosts = () => {
     type: ACTION_TYPES.REMOVE_OTHER_USER_POSTS
   }
 }
+
+export const setUserMessages = messages => {
+  return {
+    type: ACTION_TYPES.SET_USER_MESSAGES,
+    payload: messages
+  }
+}
+
+export const removeUserMessages = () => {
+  return {
+    type: ACTION_TYPES.REMOVE_USER_MESSAGES
+  }
+}
