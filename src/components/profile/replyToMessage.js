9523d750b8572365396a095af8897fa978b979be
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-22 11:22:10 -0500

    update reply message

--- src/components/profile/replyToMessage.js
+++ src/components/profile/replyToMessage.js
@@ -4,7 +4,18 @@ class ReplyToMessage extends Component {
     render() {
         return (
             <div>
-                REPLY TO MESSAGES
+                <h2>Message:</h2>
+                <div className='FlexColumn'>
+                    <div>
+                        <p>{ this.props.location.state.props.message.message_title }</p>
+                    </div>
+                    <div>
+                        <p>{ this.props.location.state.props.message.message_body }</p>
+                    </div>
+                    <div>
+                        <p>By: {this.props.location.state.props.message.message_sender}</p>
+                    </div>
+                </div>
             </div>
         );
     }
