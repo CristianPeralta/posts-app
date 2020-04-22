75ebfb1a65a17e9eb15922d82122e9f47eb97dfb
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-22 11:07:15 -0500

    add replytomessages component

---
+++ src/components/profile/replyToMessage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ReplyToMessage extends Component {
+    render() {
+        return (
+            <div>
+                REPLY TO MESSAGES
+            </div>
+        );
+    }
+}
+
+export default ReplyToMessage;
