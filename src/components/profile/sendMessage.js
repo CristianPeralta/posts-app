8df787606e11e7dbc30c0d8917044514dcd22288
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-22 10:11:10 -0500

    add send message form

--- src/components/profile/sendMessage.js
+++ src/components/profile/sendMessage.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 
+import {
+    TextField,
+    Button
+} from '@material-ui/core';
+
 class SendMessage extends Component {
     render() {
         return (
             <div>
-                SEND MESSAGE
+                <form onSubmit={() => {}}>
+                    <TextField
+                        id='title'
+                        label='title'
+                        margin='normal'
+                    />
+                    <br />
+                    <TextField
+                        id='body'
+                        multiline
+                        rows='4'
+                        margin='normal'
+                    />
+                    <br />
+                    <Button type='submit' variant='contained' color='primary' >
+                        Submit
+                    </Button>
+                    <button onClick={() => {}} >
+                        Cancel
+                    </button>
+                </form>
             </div>
         );
     }
