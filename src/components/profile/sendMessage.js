8e8c9df2e8c17d7ba5516bafc9ef8e44feb7563f
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-22 10:05:00 -0500

    add SendMessage component

---
+++ src/components/profile/sendMessage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SendMessage extends Component {
+    render() {
+        return (
+            <div>
+                SEND MESSAGE
+            </div>
+        );
+    }
+}
+
+export default SendMessage;
