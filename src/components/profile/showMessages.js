840d2fcef619ff6f1d434fa51be10b152f160f81
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-22 10:42:27 -0500

    add RenderMessages

--- src/components/profile/showMessages.js
+++ src/components/profile/showMessages.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow
+} from '@material-ui/core';
+
+const RenderMessages = props => {
+    return (
+        <div>
+            RENDER MESSAGES
+        </div>
+    );
+};
 class ShowMessages extends Component {
     render() {
         return (
             <div>
-                SHOW MESSAGES
+                <div className='FlexRow'>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell> Messages </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.props.userMessages
+                                ? this.props.userMessages.map(message =>
+                                    <RenderMessages key={message.mid} message={message}/>
+                                    )
+                                : null
+                            }
+                        </TableBody>
+                    </Table>
+                </div>
             </div>
         );
     }
