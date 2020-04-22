45031ad7a2c3b93a05247a746ca6e628456a1805
CristianPeralta <cbperaltasegura@gmail.com>
2020-04-22 10:35:47 -0500

    add showmessages component

---
+++ src/components/profile/showMessages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShowMessages extends Component {
+    render() {
+        return (
+            <div>
+                SHOW MESSAGES
+            </div>
+        );
+    }
+}
+
+export default ShowMessages;
