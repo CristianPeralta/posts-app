967ce9111d926502b3f445af8220b01993b9423b
CristianPeralta <cbperaltasegura@gmail.com>
2020-05-12 17:40:45 -0500

    add new action types

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -2,34 +2,46 @@ import * as ACTION_TYPES from './action_types';
 import axios from '../../axios';
 
 export const fetchPostsStart = () => {
-    return {
-        type: ACTION_TYPES.FETCH_POSTS_START,
-    };
+  return {
+      type: ACTION_TYPES.FETCH_POSTS_START,
   };
+};
   
-  export const fetchPostsSuccess = posts => {
-    return {
-      type: ACTION_TYPES.FETCH_POSTS_SUCCESS,
-      posts: posts
-    }
+export const fetchPostsSuccess = posts => {
+  return {
+    type: ACTION_TYPES.FETCH_POSTS_SUCCESS,
+    posts: posts,
   }
+};
   
-  export const fetchPostsFailed = (error) => {
-    return {
-        type: ACTION_TYPES.FETCH_POSTS_FAIL,
-        error: error
-    };
+export const fetchPostsFailed = (error) => {
+  return {
+      type: ACTION_TYPES.FETCH_POSTS_FAIL,
+      error: error,
   };
+};
   
-  export const fetchPosts = (query) => {
-    return dispatch => {
-        dispatch(fetchPostsStart());
-        axios.get('/posts', { params: { query }})
-            .then(response => {
-               dispatch(fetchPostsSuccess(response.data));
-            })
-            .catch(error => {
-                dispatch(fetchPostsFailed(error));
-            });
-    };
+export const fetchPosts = (query) => {
+  return dispatch => {
+      dispatch(fetchPostsStart());
+      axios.get('/posts', { params: { query }})
+          .then(response => {
+              dispatch(fetchPostsSuccess(response.data));
+          })
+          .catch(error => {
+              dispatch(fetchPostsFailed(error));
+          });
   };
+};
+
+export const addPostSuccess = () => {
+  return {
+    type: ACTION_TYPES.ADD_POST_SUCCESS,
+  }
+}
+  
+export const addPostFailed = () => {
+  return {
+      type: ACTION_TYPES.ADD_POST_FAIL,
+  };
+};
