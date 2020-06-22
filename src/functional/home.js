92ec1df8e418f0c0dc3859c29b4f9854e786369f
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-22 11:04:19 -0500

    remove reducer1

--- src/functional/home.js
+++ src/functional/home.js
@@ -3,7 +3,7 @@ import React from 'react'
 //Home page
 const Home = (props) => (
     <div>
-      Home
+      Welcome
     </div>
 );
 
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -2,12 +2,10 @@
 import AuthReducer from './auth';
 import UserReducer from './user';
 import PostReducer from './post';
-import Reducer1 from './reducer1';
 import { combineReducers } from 'redux';
 
 
 const rootReducer = combineReducers({
-  reducer1: Reducer1,
   auth: AuthReducer,
   user: UserReducer,
   post: PostReducer,
