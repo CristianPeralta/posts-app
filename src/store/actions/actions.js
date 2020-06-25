e699cff4e817ed35137eb4a32076495c40689333
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-25 13:50:21 -0500

    create auth action creators

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,4 +1,12 @@
-import * as ACTION_TYPES from './action_types';
+export {
+  logout,
+  loginFailure,
+  loginSuccess,
+  addProfile,
+  removeProfile,
+  saveProfile,
+  removeDbProfile,
+} from './auth';
 
 export {
   fetchPostsStart,
@@ -49,51 +57,3 @@ export {
   sendUserMessageSuccess,
   sendUserMessage,
 } from './user';
-
-export const logout = () => {
-  localStorage.removeItem('access_token');
-  localStorage.removeItem('id_token');
-  localStorage.removeItem('expiresAt');
-  return {
-    type: ACTION_TYPES.AUTH_LOGOUT,
-  };
-};
-
-
-export const loginSuccess = () => {
-  return {
-    type: ACTION_TYPES.LOGIN_SUCCESS
-  }
-}
-
-export const loginFailure = () => {
-  return {
-    type: ACTION_TYPES.LOGIN_FAILURE
-  }
-}
-
-export const addProfile = (profile) => {
-  return {
-    type: ACTION_TYPES.ADD_PROFILE,
-    payload: profile
-  }
-}
-
-export const removeProfile = () => {
-  return {
-    type: ACTION_TYPES.REMOVE_PROFILE
-  }
-}
-
-export const saveProfile = (profile) => {
-  return {
-    type: ACTION_TYPES.SET_DB_PROFILE,
-    payload: profile
-  }
-}
-
-export const removeDbProfile = () => {
-  return {
-    type: ACTION_TYPES.REMOVE_DB_PROFILE
-  }
-}
