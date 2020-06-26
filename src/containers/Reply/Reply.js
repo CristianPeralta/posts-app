b7fe19f27aafcf3709ccf4960928b401df30107c
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-26 12:38:59 -0500

    transform Reply class based component to functional

--- src/containers/Reply/Reply.js
+++ src/containers/Reply/Reply.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NewMessage from '../../containers/NewMessage/NewMessage';
 import PropTypes from 'prop-types';
 
-class Reply extends Component {
-    render() {
-        let message = <p>Loading...</p>
-        if (this.props.location.state.props.message) {
-            message = this.props.location.state.props.message;
-        }
-        return (
-            <div>
-                <h2>Message:</h2>
-                <div className='FlexColumn'>
-                    <div>
-                        <p>{ message.message_title }</p>
-                    </div>
-                    <div>
-                        <p>{ message.message_body }</p>
-                    </div>
-                    <div>
-                        <p>By: { message.message_sender }</p>
-                    </div>
-                </div>
-                <NewMessage location={{
-                    state: {
-                        props: {
-                            profile: {
-                                username: message.message_sender
-                            }
-                        }
-                    }
-                }}/>
-            </div>
-        );
-    }
-}
+const Reply = props => {
+  let message = <p>Loading...</p>
+  if (props.location.state.props.message) {
+      message = props.location.state.props.message;
+  }
+  return (
+      <div>
+          <h2>Message:</h2>
+          <div className='FlexColumn'>
+              <div>
+                  <p>{ message.message_title }</p>
+              </div>
+              <div>
+                  <p>{ message.message_body }</p>
+              </div>
+              <div>
+                  <p>By: { message.message_sender }</p>
+              </div>
+          </div>
+          <NewMessage location={{
+              state: {
+                  props: {
+                      profile: {
+                          username: message.message_sender
+                      }
+                  }
+              }
+          }}/>
+      </div>
+  );
+};
 
 Reply.propTypes = {
     location: PropTypes.object,
