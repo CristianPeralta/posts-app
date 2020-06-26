d7220dfc47d16bb609f9995da83788d27c890bc6
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-26 12:36:52 -0500

    transform NewMessage class based component to functional and use 'useState' hook

--- src/containers/NewMessage/NewMessage.js
+++ src/containers/NewMessage/NewMessage.js
@@ -1,65 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import * as ACTIONS from '../../store/actions/actions';
 import { Redirect } from 'react-router-dom';
 import { TextField, Button } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-class NewMessage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            to: (new URLSearchParams(this.props.location.search)).get("to"),
-            submited: false,
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleSubmit(event) {
-        event.preventDefault();
-        const data = {
-            messageTo: this.state.to,
-            messageSender: this.props.dbProfile.username,
-            messageTitle: event.target.title.value,
-            messageBody: event.target.body.value
-        };
+const NewMessage = props => {
+  const [submited, setSubmited] = useState(false);
+  const to = (new URLSearchParams(props.location.search)).get("to");
 
-        this.props.onSendUserMessage(data);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+        messageTo: to,
+        messageSender: props.dbProfile.username,
+        messageTitle: event.target.title.value,
+        messageBody: event.target.body.value
+    };
 
-        this.setState({ submited: true });
-    }
-    render() {
-        return (
-            <div>
-                {this.state.submited ? <Redirect to="/" /> : null}
-                <form onSubmit={this.handleSubmit}>
-                    <TextField
-                        id='title'
-                        label='title'
-                        margin='normal'
-                    />
-                    <br />
-                    <TextField
-                        id='body'
-                        multiline
-                        rows='4'
-                        margin='normal'
-                    />
-                    <br />
-                    <Button type='submit' variant='contained' color='primary' >
-                        Submit
-                    </Button>
-                    <button onClick={() => this.props.history.replace('/')} >
-                        Cancel
-                    </button>
-                </form>
-            </div>
-        );
-    }
-}
+    props.onSendUserMessage(data);
+    setSubmited(true);
+  };
+
+  return (
+    <div>
+        {submited ? <Redirect to="/" /> : null}
+        <form onSubmit={handleSubmit}>
+            <TextField
+                id='title'
+                label='title'
+                margin='normal'
+            />
+            <br />
+            <TextField
+                id='body'
+                multiline
+                rows='4'
+                margin='normal'
+            />
+            <br />
+            <Button type='submit' variant='contained' color='primary' >
+                Submit
+            </Button>
+            <button onClick={() => props.history.replace('/')} >
+                Cancel
+            </button>
+        </form>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
     return {
-        dbProfile: state.auth.dbProfile
+        dbProfile: state.auth.dbProfile,
     };
 };
 
