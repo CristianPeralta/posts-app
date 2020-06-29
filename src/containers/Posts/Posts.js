8300fc1238592a5aeb8a5d0922ad2d60acd91e02
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-29 14:21:23 -0500

    transform 'Posts' component to functional and use hooks

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -1,96 +1,86 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as ACTIONS from '../../store/actions/actions';
 import Pagination from 'react-js-pagination';
 import withErrorHandler from '../../hoc/withErrorHandler';
 import axios from '../../axios';
-import {
-    Button,
-    TextField,
-} from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import Post from '../../components/Post';
 import PropTypes from 'prop-types';
 
-class Posts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activePage: 1,
-            PostsPerPage: 5,
-        }
-        this.handlePageChange = this.handlePageChange.bind(this);
-        this.handleSearch = this.handleSearch.bind(this);
-    }
+const Posts = props => {
+  const [activePage] = useState(1);
+  const [postsPerPage] = useState(5);
 
-    componentDidMount() {
-        this.props.onFetchPosts();
-    }
-    handleSearch(event) {
-        const query = event.target.value;
-        this.props.onFetchPosts({query});
-    }
+  useEffect(() => {
+    props.onFetchPosts();
+  }, []);
 
-    handlePageChange(pageNumber) {
-        console.log('handlePageChange', pageNumber);
-    }
-    render() {
-        return (
+  const handleSearch = (event) => {
+    const query = event.target.value;
+    props.onFetchPosts({query});
+  };
+
+  const handlePageChange = (pageNumber) => {
+      console.log('handlePageChange', pageNumber);
+  };
+
+  return (
+    <div>
+        <div>
+            <br />
+
+            {props.isAuthenticated ?
+                <Link to='/posts/new' >
+                    <Button variant='contained' color='primary'>
+                        Add Post
+                    </Button>
+                </Link>
+                : <Link to='/signup' >
+                    <Button variant='contained' color='primary'>
+                        Signup to Add Post
+                    </Button>
+                </Link>
+            }
+        </div>
+        <div>
+            <TextField
+                id='search'
+                label='Search'
+                margin='normal'
+                onChange={handleSearch}
+            />
+        </div>
+        <div>
+            <h1>Posts</h1>
             <div>
-                <div>
-                    <br />
-
-                    {this.props.isAuthenticated ?
-                        <Link to='/posts/new' >
-                            <Button variant='contained' color='primary'>
-                                Add Post
-                            </Button>
-                        </Link>
-                        : <Link to='/signup' >
-                            <Button variant='contained' color='primary'>
-                                Signup to Add Post
-                            </Button>
-                        </Link>
-                    }
-                </div>
-                <div>
-                    <TextField
-                        id='search'
-                        label='Search'
-                        margin='normal'
-                        onChange={this.handleSearch}
-                    />
-                </div>
-                <div>
-                    <h1>Posts</h1>
-                    <div>
-                    {   this.props.posts
-                        ? this.props.posts.map(
-                                post => <Post
-                                    key={post.pid}
-                                    post={post}
-                                    showAuthor
-                                    isAuthenticated={this.props.isAuthenticated}
-                                    history={this.props.history}
-                                    profile={this.props.profile}
-                                    onAddPostLike={this.props.onAddPostLike}
-                                    />
-                        )
-                        : null
-                    }
-                    </div>
-                    <Pagination
-                        activePage={this.state.activePage}
-                        itemsCountPerPage={this.state.PostsPerPage}
-                        totalItemsCount={this.props.posts.length}
-                        pageRangeDisplayed={this.props.posts.length/this.state.PostsPerPage}
-                        onChange={this.handlePageChange}
-                    />
-                </div>
+            {   props.posts
+                ? props.posts.map(
+                        post => <Post
+                            key={post.pid}
+                            post={post}
+                            showAuthor
+                            isAuthenticated={props.isAuthenticated}
+                            history={props.history}
+                            profile={props.profile}
+                            onAddPostLike={props.onAddPostLike}
+                            />
+                )
+                : null
+            }
             </div>
-        );
-    }
-}
+            <Pagination
+                activePage={activePage}
+                itemsCountPerPage={postsPerPage}
+                totalItemsCount={props.posts.length}
+                pageRangeDisplayed={props.posts.length/postsPerPage}
+                onChange={handlePageChange}
+            />
+        </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
     return {
