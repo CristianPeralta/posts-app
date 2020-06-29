6c73e388ee3cdc49621e7f60ecc3bc4cf257754c
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-29 10:28:55 -0500

    transform 'FullPost' component to functional and use hooks

--- src/containers/FullPost/FullPost.js
+++ src/containers/FullPost/FullPost.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as ACTIONS from '../../store/actions/actions';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
@@ -9,152 +9,147 @@ import { getPost } from '../../api';
 import { Button, TextField } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-class FullPost extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            post: null,
-            open: false,
-            comment: '',
-            cid: '',
-            opacity: 1,
-        }
-        this.handleClickOpen = this.handleClickOpen.bind(this);
-        this.handleCommentChange = this.handleCommentChange.bind(this);
-        this.handleDelete = this.handleDelete.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleUpdate = this.handleUpdate.bind(this);
-        this.handleLikes = this.handleLikes.bind(this);
-    }
+const FullPost = props => {
+    const [post, setPost] = useState(null);
+    const [open, setOpen] = useState(false);
+    const [comment, setComment] = useState('');
+    const [cid, setCid] = useState('');
+    const [opacity] = useState(1);
 
-    componentDidMount() {
-        getPost(this.props.match.params.pid)
-            .then(post => this.setState({ post: post }))
-            .then(() => this.props.onFetchPostComments({ pid: this.props.match.params.pid }))
+    useEffect(() => {
+        getPost(props.match.params.pid)
+            .then(post => setPost(post))
+            .then(() => props.onFetchPostComments({ pid: props.match.params.pid }))
             .catch(error => console.log(error));
-    }
+    }, []);
 
-    handleClickOpen(cid, comment) {
-        this.setState({ open: true, comment: comment, cid: cid });
-    }
-    handleClose() {
-        this.setState({ open: false, comment: '', cid: '' });
-    }
-    handleCommentChange(event) {
-        this.setState({ comment: event.target.value });
-    }
+    const handleClickOpen = (cid, comment) => {
+        setOpen(true);
+        setComment(comment);
+        setCid(cid);
+    };
 
-    handleSubmit(event) {
+    const handleClose = () => {
+        setOpen(false);
+        setComment('');
+        setCid('');
+    };
+
+    const handleCommentChange = (event) => {
+        setComment(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
         event.preventDefault();
         const data = {
             comment: event.target.comment.value,
-            postId: this.state.post.pid,
-            userId: this.props.profile.uid,
-            username: this.props.profile.username,
+            postId: post.pid,
+            userId: props.profile.uid,
+            username: props.profile.username,
         };
 
-        this.props.onAddPostComment(data);
+        props.onAddPostComment(data);
 
         window.scroll({top: 0, left: 0, behavior: 'smooth'});
-    }
-    handleUpdate() {
-        this.setState({comment: ''});
+    };
+
+    const handleUpdate = () => {
+        setComment('');
         const data = {
-            cid: this.state.cid,
-            comment: this.state.comment,
-            postId: this.state.post.pid,
-            userId: this.props.profile.uid,
-            username: this.props.profile.username,
+            cid: cid,
+            comment: comment,
+            postId: post.pid,
+            userId: props.profile.uid,
+            username: props.profile.username,
         };
 
-        this.props.onEditPostComment(data);
-    }
-    handleDelete() {
-        const cid = this.state.cid;
-        this.props.onDeletePostComment(cid);
-    }
-    handleLikes(pid) {
+        props.onEditPostComment(data);
+    };
+
+    const handleDelete = () => {
+        props.onDeletePostComment(cid);
+    };
+
+    const handleLikes = (pid) => {
         const data = {
-            uid: this.props.profile.uid,
+            uid: props.profile.uid,
             postId: pid,
         };
-        this.props.onAddPostLike(data);
+        props.onAddPostLike(data);
+    };
+
+    let renderPost = <p>Loading</p>;
+    if (post) {
+        renderPost = <Post
+            key={post.pid}
+            post={post}
+            showAuthor
+            isAuthenticated={props.isAuthenticated}
+            history={props.history}
+            profile={props.profile}
+            onAddPostLike={props.onAddPostLike}
+            onHandleLikes={handleLikes}
+        />;
     }
-    render() {
-        let post = <p>Loading</p>;
-        if (this.state.post) {
-            post = <Post
-                key={this.state.post.pid}
-                post={this.state.post}
-                showAuthor
-                isAuthenticated={this.props.isAuthenticated}
-                history={this.props.history}
-                profile={this.props.profile}
-                onAddPostLike={this.props.onAddPostLike}
-                onHandleLikes={this.handleLikes}
-            />;
-        }
-        return (
-            <div>
-                {post}
-                <div style={{opacity: this.state.opacity, transition: 'ease-out 2s' }} >
-                    <h2>Comments</h2>
-                    {this.props.comments
-                    ? this.props.comments.map(comment => (
-                        <Comment
-                            key={comment.cid}
-                            comment={comment}
-                            edit={this.handleClickOpen}
-                            userId={this.props.profile ? this.props.profile.uid : null}
-                            commentStyle={this.state.deleteCommentId === comment.cid
-                                ? "FadeOutComment"
-                                : "CommentStyles"
-                            }
-                            />
-                        )) : null
-                    }
-                </div>
-                <div>
-                    <form onSubmit={e => this.handleSubmit(e)} >
-                        <TextField
-                            id='comment'
-                            label='Comment'
-                            margin='normal'
-                        />
-                        <br />
-                        {this.props.isAuthenticated
-                            ? <Button type='submit'>Submit</Button>
-                            : <Link to='/signup'>
-                                <Button color='primary' variant='contained' >
-                                    Signup to Comment
-                                </Button>
-                              </Link>
+    return (
+        <div>
+            {renderPost}
+            <div style={{opacity: opacity, transition: 'ease-out 2s' }} >
+                <h2>Comments</h2>
+                {props.comments
+                ? props.comments.map(comment => (
+                    <Comment
+                        key={comment.cid}
+                        comment={comment}
+                        edit={handleClickOpen}
+                        userId={props.profile ? props.profile.uid : null}
+                        commentStyle={cid === comment.cid
+                            ? "FadeOutComment"
+                            : "CommentStyles"
                         }
-                    </form>
-                </div>
-                <div>
-                <ModalDialog
-                    title="Edit Comment"
-                    open={this.state.open}
-                    close={this.handleClose}
-                    agreeAction={() => {
-                        this.handleUpdate();
-                        this.setState({ open: false });
-                    }}
-                    cancelAction={this.handleClose}
-                    deleteAction={() => {
-                        this.handleDelete();
-                        this.setState({ open: false });
-                    }}
-                >
-                    <input type='text' value={this.state.comment} onChange={this.handleCommentChange} />
-                </ModalDialog>
-                </div>
+                        />
+                    )) : null
+                }
             </div>
-        );
-    }
-}
+            <div>
+                <form onSubmit={e => handleSubmit(e)} >
+                    <TextField
+                        id='comment'
+                        label='Comment'
+                        margin='normal'
+                    />
+                    <br />
+                    {props.isAuthenticated
+                        ? <Button type='submit'>Submit</Button>
+                        : <Link to='/signup'>
+                            <Button color='primary' variant='contained' >
+                                Signup to Comment
+                            </Button>
+                          </Link>
+                    }
+                </form>
+            </div>
+            <div>
+            <ModalDialog
+                title="Edit Comment"
+                open={open}
+                close={handleClose}
+                agreeAction={() => {
+                    handleUpdate();
+                    setOpen(false);
+                }}
+                cancelAction={handleClose}
+                deleteAction={() => {
+                    handleDelete();
+                    setOpen(false);
+                }}
+            >
+                <input type='text' value={comment} onChange={handleCommentChange} />
+            </ModalDialog>
+            </div>
+        </div>
+    );
+};
 
 const mapStateToProps = state => {
     return {
