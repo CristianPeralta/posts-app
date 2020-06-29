c94e61f9de85a00edbfeb3ac34811835d8f1d772
CristianPeralta <cbperaltasegura@gmail.com>
2020-06-29 15:38:04 -0500

    transform 'Profile' component to functional and use hooks

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as ACTIONS from '../store/actions/actions';
@@ -9,33 +9,23 @@ import ModalDialog from '../components/UI/ModalDialog';
 import { Button } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
-class Profile extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      redirectToHome: false,
-      open: false,
-      postId: ''
-    }
-  }
-  componentDidMount() {
-    if (this.props.dbProfile) {
-      this.loadPosts();
-    }
-  }
+const Profile = props => {
+  const [redirectToHome] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [postId, setPostId] = useState('');
 
-  loadPosts = () => {
-    const userId = this.props.dbProfile.uid;
-    this.props.onFetchUserPosts({ userId: userId });
-  }
-  shouldComponentUpdate(nextProps, nextStates) {
-    const shouldUpdate = (nextProps.dbProfile && !this.props.dbProfile) 
-      || !(nextProps.dbProfile && this.props.dbProfile && nextProps.dbProfile.uid === this.props.dbProfile.uid
-      && nextProps.userPosts.length === this.props.userPosts.length);
-    return shouldUpdate || JSON.stringify(this.state) !== JSON.stringify(nextStates);
-  }
+  const loadPosts = useCallback((userId) => {
+    props.onFetchUserPosts({ userId: userId });
+  }, [props.onFetchUserPosts, props.dbProfile]);
 
-  RenderProfile = (props) => (
+  useEffect(() => {
+    if (props.dbProfile) {
+      const userId = props.dbProfile.uid;
+      loadPosts(userId);
+    }
+  }, [props.dbProfile]);
+
+  const RenderProfile = (props) => (
     <div>
       <h1>{props.profile.nickname}</h1>
       <br />
@@ -49,78 +39,78 @@ class Profile extends Component {
       {props.profile.emailVerified ? <p>Yes</p> : <p>No</p> }
       <br />
     </div>
-  )
+  );
 
-  DeletePost = () => {
-    const postId = this.state.postId;
-    this.props.onDeletePost(postId);
-    this.handleClickClose();
-  }
+  const deletePost = () => {
+    props.onDeletePost(postId);
+    handleClickClose();
+  };
 
-  handleClickOpen = pid => {
-    this.setState({ open: true, postId: pid });
-  }
+  const handleClickOpen = pid => {
+    setOpen(true);
+    setPostId(pid);
+  };
 
-  handleClickClose = () => {
-    this.setState({ open: false, postId: null });
-  }
-  render() {
-    return(
+  const handleClickClose = () => {
+    setOpen(false);
+    setPostId(null);
+  };
+
+  return(
+    <div>
+      {redirectToHome ? <Redirect to='/' /> : null}
       <div>
-        {this.state.redirectToHome ? <Redirect to='/' /> : null}
-        <div>
-          {this.props.dbProfile ? <this.RenderProfile profile={this.props.profile} /> : <p>Loading</p>} 
-        </div>
-        <div>
-          {
-            this.props.dbProfile ? <Link to={{pathname: '/messages/' + this.props.dbProfile.uid}}>
-            <Button variant='contained' color='primary' type='submit'>
-                Show Messages
-            </Button>
-          </Link>
-          : <p>Loading</p>
-          }
-        </div>
-        {this.props.dbProfile ? this.loadPosts() : null}
-        <div>
-          {
-            this.props.userPosts ?
-              this.props.userPosts.map(post => (
-                <Post post={post} key={post.pid} showAuthor={false} >
-                  <div className='FlexRow'>
-                    <Link to={{ pathname: '/posts/' + post.pid + '/edit'}}>
-                      <button>
-                        Edit
-                      </button>
-                    </Link>
-                    <button onClick={() => this.handleClickOpen(post.pid)} >
-                      Delete
-                    </button>
-                  </div>
-                </Post> )) : null
-          }
-        </div>
-        <ModalDialog
-          title="Delete Post"
-          open={this.state.open}
-          close={this.handleClickClose}
-          agreeAction={this.DeletePost}
-          cancelAction={this.handleClickClose}
-        >
-          Deleting Post
-        </ModalDialog>
+        {props.dbProfile ? <RenderProfile profile={props.profile} /> : <p>Loading</p>}
       </div>
-    );
-  }
-}
+      <div>
+        {
+          props.dbProfile ? <Link to={{pathname: '/messages/' + props.dbProfile.uid}}>
+          <Button variant='contained' color='primary' type='submit'>
+              Show Messages
+          </Button>
+        </Link>
+        : <p>Loading</p>
+        }
+      </div>
+      {}
+      <div>
+        {
+          props.userPosts ?
+            props.userPosts.map(post => (
+              <Post post={post} key={post.pid} showAuthor={false} >
+                <div className='FlexRow'>
+                  <Link to={{ pathname: '/posts/' + post.pid + '/edit'}}>
+                    <button>
+                      Edit
+                    </button>
+                  </Link>
+                  <button onClick={() => handleClickOpen(post.pid)} >
+                    Delete
+                  </button>
+                </div>
+              </Post> )) : null
+        }
+      </div>
+      <ModalDialog
+        title="Delete Post"
+        open={open}
+        close={handleClickClose}
+        agreeAction={deletePost}
+        cancelAction={handleClickClose}
+      >
+        Deleting Post
+      </ModalDialog>
+    </div>
+  );
+};
 
-function mapStateToProps(state) {
+const mapStateToProps = (state) => {
   return {
     profile: state.auth.profile,
     dbProfile: state.auth.dbProfile,
     userPosts: state.post.posts,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
